0822fb4f33b8684b128e8ce7494508314403b19d
Juan Camilo Medina <milomed2012@gmail.com>
2026-09-04 21:43:14 -0500

    Configurar PyMySQL con DictCursor

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def obtener_conexion():
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"],
         database=os.environ["DB_NAME"],
-        port=int(os.environ.get("DB_PORT", 3306))
+        port=int(os.environ.get("DB_PORT", 3306)),
+        cursorclass=pymysql.cursors.DictCursor
     )
 
 lista_tickets = [
